7d2b096bdb0d9b926dcc06c6d9e5d6ce0502c5fb
fzh <1209103220@qq.com>
2021-08-28 11:19:06 +0800

    文件存储状态监控

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,14 @@ ADD . /mnt/dool
 Workdir /mnt/dool
 # docker build -f Dockerfile -t registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:dool .
 # docker push registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:dool
-# docker run -it --rm --name=dool registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:dool /bin/bash
+# docker run -it --rm -v /Users/fzh/project/fzh/dool:/mnt/dool --name=dool registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:dool /bin/bash
 
 
 # 查看所有插件
 # /mnt/dool/dool --list
 
-# 
+# ./dool -c -C 0,1,total
+
+# ./dool --filecoin-sectors-status
+
+# python3 ./dool --filecoin-sectors-status -c -C 0,1
--- plugins/dool_filecoin_sectors_status.py
+++ plugins/dool_filecoin_sectors_status.py
@@ -1,55 +1,63 @@
+
 from websocket import create_connection
 import json
-
-def websocket(message):
-    ws = create_connection("ws://192.168.10.102:2345/rpc/v0")
-    # ws = websocket.create_connection("ws://192.168.10.102:2345/rpc/v0")
-    ws.send(message)
-    return ws.recv()
-
-def lotusMinerSectorsList():
-    data = {
-        "jsonrpc": "2.0",
-        "id": 1,
-        "method": "Filecoin.SectorsList",
-        "params": [],
-        "meta": {
-        }
-    }
-    res = websocket(json.dumps(data))
-    res_json = json.loads(res)
-    res_json_list = res_json["result"]
-    res_list = list(map(lotusMinerSectorsInfo,res_json_list))
-    return res_list
-
-def lotusMinerSectorsInfo(i):
-    data = {
-        "jsonrpc": "2.0",
-        "id": 1,
-        "method": "Filecoin.SectorsStatus",
-        "params": [
-            i,
-            True
-        ],
-        "meta": {
-        }
-    }
-    res = websocket(json.dumps(data))
-    res_json = json.loads(res)
-    return res_json["result"]
-
 class  dstat_plugin(dstat):
      """
      Total Number of processes on this system.
      """
      def  __init__( self ):
+         self .id = 'filecoin-sectors-status'
+         self .type  =  's'
+
          self .name    =  'filecoin-sectors-status'
-         self .type  =  'd'
-         self .width  =  4
-         self .scale  =  10
+         self .nick  = ( 'SectorID', 'State')
+         self .width  =  5
+         self .scale  =  1000
          self .vars    =  ( 'total' ,'work')
  
      def  extract( self ): 
-         self .val[ 'total' ]  =  len (glob.glob( '/proc/[0-9]*' ))
-         self .val[ 'work' ]  =  len (glob.glob( '/proc/[0-9]*' ))
-         self .val[ 'ccc' ]  =  "cc"
+         self .data = self.lotusMinerSectorsList()
+         self .vars = list(map(lambda e:e["SectorID"],self.data))
+         self .name = []
+         for name in self.vars:
+             self .name.append('sectors' + str(name) + ' state')
+
+         self .val = {}
+         list(map(lambda e:self .val.setdefault(e["SectorID"],[str(e["SectorID"]),e["State"]]),self.data))
+
+     def websocket(self,message):
+         ws = create_connection("ws://192.168.10.102:2345/rpc/v0")
+         # ws = websocket.create_connection("ws://192.168.10.102:2345/rpc/v0")
+         ws.send(message)
+         return ws.recv()
+
+     def lotusMinerSectorsList(self):
+        data = {
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "Filecoin.SectorsList",
+            "params": [],
+            "meta": {
+            }
+        }
+        res = self.websocket(json.dumps(data))
+        res_json = json.loads(res)
+        res_json_list = res_json["result"]
+        res_list = list(map(self.lotusMinerSectorsInfo,res_json_list))
+        return res_list
+
+     def lotusMinerSectorsInfo(self,i):
+         data = {
+             "jsonrpc": "2.0",
+             "id": 1,
+             "method": "Filecoin.SectorsStatus",
+             "params": [
+                 i,
+                 True
+             ],
+             "meta": {
+             }
+         }
+         res = self.websocket(json.dumps(data))
+         res_json = json.loads(res)
+         return res_json["result"]
