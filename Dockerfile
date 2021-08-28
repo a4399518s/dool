fc8d832d11111c71ba609e90810cfad0d6491639
fzh <1209103220@qq.com>
2021-08-28 09:17:33 +0800

    file coin 监控

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,17 @@
 from registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:7
 RUN mkdir /mnt/dool
+RUN pip3 install kafka-python -i https://mirrors.aliyun.com/pypi/simple/ 
+RUN pip3 install websocket-client -i https://mirrors.aliyun.com/pypi/simple/ 
+
+ADD . /mnt/dool
+
 Workdir /mnt/dool
 # docker build -f Dockerfile -t registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:dool .
 # docker push registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:dool
-# docker run -it --rm -v /Users/fzh/project/fzh/dool:/mnt/dool --name=dool registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:dool /bin/bash
+# docker run -it --rm --name=dool registry.cn-shanghai.aliyuncs.com/fengzhihao/centos:dool /bin/bash
 
 
+# 查看所有插件
 # /mnt/dool/dool --list
+
+# 
--- plugins/dool_filecoin_sectors_status.py
+++ plugins/dool_filecoin_sectors_status.py
@@ -1,14 +1,55 @@
+from websocket import create_connection
+import json
+
+def websocket(message):
+    ws = create_connection("ws://192.168.10.102:2345/rpc/v0")
+    # ws = websocket.create_connection("ws://192.168.10.102:2345/rpc/v0")
+    ws.send(message)
+    return ws.recv()
+
+def lotusMinerSectorsList():
+    data = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "Filecoin.SectorsList",
+        "params": [],
+        "meta": {
+        }
+    }
+    res = websocket(json.dumps(data))
+    res_json = json.loads(res)
+    res_json_list = res_json["result"]
+    res_list = list(map(lotusMinerSectorsInfo,res_json_list))
+    return res_list
+
+def lotusMinerSectorsInfo(i):
+    data = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "Filecoin.SectorsStatus",
+        "params": [
+            i,
+            True
+        ],
+        "meta": {
+        }
+    }
+    res = websocket(json.dumps(data))
+    res_json = json.loads(res)
+    return res_json["result"]
+
 class  dstat_plugin(dstat):
      """
      Total Number of processes on this system.
      """
      def  __init__( self ):
-         self .name    =  'fzh_hello'
+         self .name    =  'filecoin-sectors-status'
          self .type  =  'd'
          self .width  =  4
          self .scale  =  10
          self .vars    =  ( 'total' ,'work')
  
-     def  extract( self ):
+     def  extract( self ): 
          self .val[ 'total' ]  =  len (glob.glob( '/proc/[0-9]*' ))
          self .val[ 'work' ]  =  len (glob.glob( '/proc/[0-9]*' ))
+         self .val[ 'ccc' ]  =  "cc"
